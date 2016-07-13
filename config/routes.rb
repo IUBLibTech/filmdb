8b43f2b7643072217035428a0209ecaa185a3e0f
Andrew Albrecht <jaalbrec@indiana.edu>
2016-07-13 10:54:07 -0400

    adding root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "physical_objects#index"
+
   resources :physical_objects
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
