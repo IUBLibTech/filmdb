007c3e9d1b37a7b56fa957a34eb1db7f54b39876
Andrew Albrecht <jaalbrec@indiana.edu>
2017-05-25 14:12:55 -0400

    fixed a bug in migration file

--- db/migrate/20170522191735_first_change_to_workflow_statuses.rb
+++ db/migrate/20170522191735_first_change_to_workflow_statuses.rb
@@ -3,7 +3,7 @@ class FirstChangeToWorkflowStatuses < ActiveRecord::Migration
     WorkflowStatus.all.delete_all
     WorkflowStatusTemplate.all.delete_all
     ['In Storage', 'Queued for Pull Request', 'Pull Requested', 'On Site', 'Shipped External' ].each_with_index do |s, i|
-      WorkflowStatusTemplate.new(type_and_location: s, sort_order: i, description: '').save
+      WorkflowStatusTemplate.new(name: s, sort_order: i, description: '').save
     end
   end
 end
