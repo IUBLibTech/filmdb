61323cb46b724a941919edbbab8e629f994d4b8a
Andrew Albrecht <jaalbrec@indiana.edu>
2017-09-11 09:28:03 -0400

    added release information to the env file and application view

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Use tag or commit hash as version string; sub removes tag from tag-hash output
+APP_VERSION = `git describe --always`.sub(/.*-g/, '') unless defined? APP_VERSION
+
+
 # Initialize the Rails application.
 Rails.application.initialize!
