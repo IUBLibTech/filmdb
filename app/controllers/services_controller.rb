21f73ed508bdd69f140abcd6357b1f288e72f825
Andrew Albrecht <jaalbrec@indiana.edu>
2018-01-05 10:48:10 -0500

    fixed POD returned to storage call failure

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,6 +1,9 @@
 class ServicesController < ApplicationController
 	require 'net/http'
 	require 'uri'
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :null_session
 	include CagesHelper
 	include BasicAuthenticationHelper
 
