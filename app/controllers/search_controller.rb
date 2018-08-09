0f7712a62937b7451e3b875ad8b0c04d738b1dd8
Andrew <jaalbrec@indiana.edu>
2018-08-09 13:59:43 -0400

    added search on old barcodes, upadted metadata displayed during component group creation/edit, shift clicking POs during CG

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,9 @@ class SearchController < ApplicationController
 
 	def barcode_search
 		@physical_object = PhysicalObject.where("iu_barcode = ? OR mdpi_barcode = ?", params[:barcode], params[:barcode]).first
+		if @physical_object.nil?
+			@physical_object = PhysicalObjectOldBarcode.includes(:physical_object).where(iu_barcode: params[:barcode]).first&.physical_object
+		end
 		if @physical_object
 			render 'physical_objects/show'
 		else
