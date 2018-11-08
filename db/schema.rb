b38c4b5bc630a02df0df860d6cfae851ce96737e
Andrew <jaalbrec@indiana.edu>
2018-11-08 11:17:11 -0500

    added addition CV for language types, improved sort order of PO can size, increased font size of title title_text attribute on title#show

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180925193836) do
+ActiveRecord::Schema.define(version: 20181108155022) do
 
   create_table "boolean_conditions", force: :cascade do |t|
     t.integer  "physical_object_id", limit: 8
