14acba4dca66e82788293f8e6746964ef832aaf6
Andrew Albrecht <jaalbrec@indiana.edu>
2020-01-29 12:56:32 -0500

    Finally fixed the mdpi validation handling acts_as???

--- app/models/mdpi_barcode_validator.rb
+++ app/models/mdpi_barcode_validator.rb
@@ -3,13 +3,15 @@ class MdpiBarcodeValidator < ActiveModel::EachValidator
 	def validate_each(record, attribute, value)
 		unless value.blank?
 			assigned = ApplicationHelper.mdpi_barcode_assigned?(value)
+			debugger
 			if !ApplicationHelper.valid_barcode?(value, true)
 				record.errors.add(attribute, options[:message] || "is not valid.")
 			elsif assigned && (assigned.is_a?(CageShelf) && assigned != record)
 				record.errors.add(attribute, option[:message] || error_message_link(assigned))
-			elsif assigned && assigned.is_a?(PhysicalObject) && record != assigned
+			# the record is a base physical object and not a specific type (cage packing does this)
+			elsif assigned && assigned.is_a?(PhysicalObject) && !record.class.method_defined?(:acting_as) && record != assigned
 				record.errors.add(attribute, options[:message] || error_message_link(assigned))
-			elsif assigned && (!assigned.is_a?(PhysicalObject) || !assigned.is_a?(CageShelf))
+			elsif assigned && assigned.is_a?(PhysicalObject) && record.class.method_defined?(:acting_as) && record.acting_as != assigned
 				record.errors.add(attribute, options[:message] || error_message_link(assigned))
 			end
 		end
