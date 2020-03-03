b8bfbd4ca65e31464d8440aeab406260ada4388e
Andrew Albrecht <jaalbrec@indiana.edu>
2020-03-03 15:04:02 -0500

    Added belongs_to to TitleX objects so that delete logger works properly

--- app/models/title_creator.rb
+++ app/models/title_creator.rb
@@ -1,5 +1,5 @@
 class TitleCreator < ActiveRecord::Base
-
+  belongs_to :title
   def ==(another)
     another.class == self.class && self.role == another.role && self.name == another.name
   end
--- app/models/title_publisher.rb
+++ app/models/title_publisher.rb
@@ -1,5 +1,5 @@
 class TitlePublisher < ActiveRecord::Base
-
+  belongs_to :title
   def ==(another)
     self.class == another.class && self.name == another.name && self.publisher_type == another.publisher_type
   end
