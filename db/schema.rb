9e2ffe1c2ccd81f23dc1973e3335af43ee44145e
Andrew Albrecht <jaalbrec@indiana.edu>
2021-06-01 14:25:03 -0400

    migration for digitized status of Recorded Sound and Video formats

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210426174351) do
+ActiveRecord::Schema.define(version: 20210527184624) do
 
   create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
     t.string   "value",      limit: 255
