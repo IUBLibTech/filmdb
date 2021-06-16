05db83578bf3732b0b33ec74861d4ab8596f7d9e
Andrew Albrecht <jaalbrec@indiana.edu>
2021-06-16 09:53:18 -0400

    Bugfix for non-existent cage_shelf for RecordedSound/Video PhysicalObject digiprov view

--- lib/memnon_digiprov_collector.rb
+++ lib/memnon_digiprov_collector.rb
@@ -45,7 +45,7 @@ class MemnonDigiprovCollector
   def get(url)
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
-    #http.use_ssl = true
+    http.use_ssl = true
     request = Net::HTTP::Get.new(uri.request_uri)
     request.basic_auth(Settings.pod_qc_user, Settings.pod_qc_pass)
     http.request(request)
