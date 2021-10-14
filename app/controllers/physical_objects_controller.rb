37f955a0e9d1ce3a8deef9d9c40f6fd6d4be22b6
Andrew Albrecht <jaalbrec@indiana.edu>
2021-10-14 10:46:58 -0400

    Reverted physical_object#new to create a Film object by default (instead of EquipmentTechnology for testing)

--- app/controllers/physical_objects_controller.rb
+++ app/controllers/physical_objects_controller.rb
@@ -83,8 +83,7 @@ class PhysicalObjectsController < ApplicationController
     u = User.current_user_object
     if request.get?
       @em = "Creating New Physical Object"
-      #@physical_object = Film.new(inventoried_by: u.id, modified_by: u.id, media_type: 'Moving Image', medium: 'Film')
-      @physical_object = EquipmentTechnology.new(inventoried_by: u.id, modified_by: u.id, media_type: '', medium: 'Equipment/Technology')
+      @physical_object = Film.new(inventoried_by: u.id, modified_by: u.id, media_type: 'Moving Image', medium: 'Film')
       set_cv
     elsif request.post?
       new_one = blank_specific_po(medium_value_from_params)
